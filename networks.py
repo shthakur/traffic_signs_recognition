9e3b0dd8720e27c4b9295edbb25ca6eaf3de3c48
Shruti Thakur <thakur.shruti612@gmail.com>
2021-05-07 16:59:00 -0800

    Fix issue with the network

--- networks.py
+++ networks.py
@@ -18,21 +18,27 @@ def new_conv(input_c, out_c, filter_size=5, padding_size=2,
     return model
 
 
-def new_fc(input_nbr, out_nbr):
-    model = nn.Sequential(
-        nn.Linear(input_nbr, out_nbr),
-        nn.ReLU()
-    )
+class FC(nn.Module):
+    def __init__(self, input_nbr, out_nbr):
+        super(FC, self).__init__()
+        self.input_nbr = input_nbr
+        self.lin = nn.Linear(input_nbr, out_nbr)
+        self.rel = nn.ReLU()
 
-    return model
+    def forward(self, input):
+        # input = input.view(-1, self.input_nbr)
+        return self.rel(self.lin(input))
 
 
-def new_classifier(input_nbr, out_nbr):
-    model = nn.Sequential(
-        nn.Linear(input_nbr, out_nbr)
-    )
+class Classifier(nn.Module):
+    def __init__(self, input_nbr, out_nbr):
+        super(Classifier, self).__init__()
+        self.input_nbr = input_nbr
+        self.lin = nn.Linear(input_nbr, out_nbr)
 
-    return model
+    def forward(self, input):
+        # input = input.view(-1, self.input_nbr)
+        return self.lin(input)
 
 
 def get_convs_output_size(network, input_size=BASE_INPUT_SIZE):
@@ -43,13 +49,12 @@ def get_convs_output_size(network, input_size=BASE_INPUT_SIZE):
 
     in_channels = network[0][0].in_channels
 
-    output = Variable(torch.Tensor(1, in_channels, input_size, input_size))
+    output = Variable(torch.ones(1, in_channels, input_size, input_size))
     output.require_grad = False
-
     for conv in network:
         output = conv.forward(output)
 
-    return np.prod(output.data.shape), output.data.size()[2]
+    return np.asscalar(np.prod(output.data.shape)), output.data.size()[2]
 
 
 class LocalizationNetwork(nn.Module):
@@ -62,21 +67,22 @@ class LocalizationNetwork(nn.Module):
         self.conv1 = new_conv(input_channels, conv_params[0])
         self.conv2 = new_conv(conv_params[0], conv_params[1])
         conv_output_size, _ = get_convs_output_size([self.conv1, self.conv2],
-                                                    input_size / 2)
-        self.fc = new_fc(conv_output_size, conv_params[2])
-        self.classifier = new_classifier(conv_params[2], nbr_params)
-        self.classifier.weight.data.zero_()
-        self.classifier.bias.data.copy_(init_bias)
+                                                    input_size // 2)
+
+        self.fc = FC(conv_output_size, conv_params[2])
+        self.classifier = Classifier(conv_params[2], self.nbr_params)
+        self.classifier.lin.weight.data.zero_()
+        self.classifier.lin.bias.data.copy_(self.init_bias)
 
     def forward(self, input):
+        input = nn.functional.max_pool2d(input, 2)
         conv_output = self.conv2(self.conv1(input))
         conv_output = conv_output.view(conv_output.size()[0], -1)
-
         return self.classifier(self.fc(conv_output))
 
 
 class SpatialTransformer(nn.Module):
-    def __init__(self, params, input_size=BASE_INPUT_SIZE
+    def __init__(self, params, input_size=BASE_INPUT_SIZE,
                  input_channels=3):
         super(SpatialTransformer, self).__init__()
         conv_params = list(map(int, params.split(",")))
@@ -92,14 +98,14 @@ class SpatialTransformer(nn.Module):
 
 
 class GeneralNetwork(nn.Module):
-    def __init__(opt):
-        super(Network, self).__init__()
+    def __init__(self, opt):
+        super(GeneralNetwork, self).__init__()
         conv_params = list(map(int, opt.cnn.split(",")))
         self.conv1 = new_conv(3, conv_params[0])
         self.conv2 = new_conv(conv_params[0], conv_params[1])
         conv_output_size, _ = get_convs_output_size([self.conv1, self.conv2])
-        self.fc = new_fc(conv_output_size, conv_params[2])
-        self.classifier = new_classifier(conv_params[2], NUM_CLASSES)
+        self.fc = FC(conv_output_size, conv_params[2])
+        self.classifier = Classifier(conv_params[2], NUM_CLASSES)
         self.locnet_1 = None
         if opt.st and opt.locnet:
             self.locnet_1 = SpatialTransformer(opt.locnet)
@@ -124,15 +130,15 @@ class GeneralNetwork(nn.Module):
 
 
 class IDSIANetwork(GeneralNetwork):
-    def __init__(opt):
+    def __init__(self, opt):
         super().__init__(opt)
         conv_params = list(map(int, opt.cnn.split(",")))
         self.conv3 = new_conv(conv_params[1], conv_params[2])
         conv_output_size, _ = get_convs_output_size([self.conv1,
                                                      self.conv2,
                                                      self.conv3])
-        self.fc = new_fc(conv_output_size, conv_params[3])
-        self.classifier = new_classifier(conv_params[3], NUM_CLASSES)
+        self.fc = FC(conv_output_size, conv_params[3])
+        self.classifier = Classifier(conv_params[3], NUM_CLASSES)
 
         self.locnet_3 = None
         if opt.st and opt.locnet3:
@@ -154,4 +160,7 @@ class IDSIANetwork(GeneralNetwork):
 
         if self.locnet_3:
             input = self.locnet_3(input)
-        return self.classifier(self.fc(self.conv3(input)))
+        input = self.conv3(input)
+
+        input = input.view(input.size()[0], -1)
+        return self.classifier(self.fc(input))
