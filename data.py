45b81e95d6bfafa552852f470b42c392973fd458
Shruti Thakur <thakur.shruti612@gmail.com>
2021-05-08 19:19:00 -0800

    Add data transform for normalizing and brightness

--- data.py
+++ data.py
@@ -3,15 +3,16 @@ import zipfile
 import os
 
 import torchvision.transforms as transforms
-
+from transforms import preprocess_img
 # once the images are loaded, how do we pre-process them before being passed into the network
 # by default, we resize the images to 32 x 32 in size
 # and normalize them to mean = 0 and standard-deviation = 1 based on statistics collected from
 # the training set
 data_transforms = transforms.Compose([
-    transforms.Scale((48, 48)),
+    # transforms.Scale((48, 48)),
+    transforms.Lambda(lambda x: preprocess_img(x)),
     transforms.ToTensor(),
-    transforms.Normalize((0.3337, 0.3064, 0.3171), ( 0.2672, 0.2564, 0.2629))
+    # transforms.Normalize((0.3337, 0.3064, 0.3171), ( 0.2672, 0.2564, 0.2629))
 ])
 
 
---
+++ transforms.py
@@ -0,0 +1,35 @@
+import numpy as np
+from skimage import color, exposure, transform
+from constants import IMG_SIZE
+from PIL import Image
+import cv2
+
+
+def preprocess_img(data):
+    img = np.array(data)
+    cv2.imshow('image', img)
+    cv2.waitKey()
+    # Histogram normalization in v channel
+    hsv = color.rgb2hsv(img)
+    hsv[:, :, 2] = exposure.equalize_hist(hsv[:, :, 2])
+    img = color.hsv2rgb(hsv)
+
+    # central square crop
+    min_side = min(img.shape[:-1])
+    centre = img.shape[0] // 2, img.shape[1] // 2
+    img = img[centre[0] - min_side // 2:centre[0] + min_side // 2,
+              centre[1] - min_side // 2:centre[1] + min_side // 2,
+              :]
+
+    # rescale to standard size
+    img = transform.resize(img, (IMG_SIZE, IMG_SIZE), mode='constant')
+
+    # roll color axis to axis 0
+    # img = np.rollaxis(img, -1)
+    return Image.fromarray(np.uint8(img * 255))
+
+
+def data_test(img_path):
+    img = Image.open(img_path)
+    cv2.imshow('image', np.array(preprocess_img(img)))
+    cv2.waitKey()
